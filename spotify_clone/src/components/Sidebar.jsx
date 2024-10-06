9157e3ad03fa6b45bf2981dc15e515424dc36a3e
arun909 <arunbabu7591@gmail.com>
2024-10-07 02:18:14 +0530

    playlist half

--- spotify_clone/src/components/Sidebar.jsx
+++ spotify_clone/src/components/Sidebar.jsx
@@ -1,11 +1,77 @@
 import React from 'react'
 import styled from 'styled-components'  
-export default function m() {
-  return <Container>Sidebar</Container>;
-    <div>Sidebar</div>
+import{ IoLibrary } from "react-icons/io5";
+import{ MdHomeFilled, MdSearch } from "react-icons/md";
+import Playlist from './Playlist';
 
+
+export default function m() {
+  return <Container>
+    <div className='top_links'>
+        <div className='logo'>
+        <img src="https://storage.googleapis.com/pr-newsroom-wp/1/2023/05/Spotify_Full_Logo_RGB_White.png" 
+    alt="spotify" 
+    />
+        </div>
+        <ul>
+            <li>
+                 <MdHomeFilled />
+                <span>Home</span>
+                </li>
+            <li> 
+                <MdSearch />
+                <span>Search</span>
+            </li>
+            <li>
+                <IoLibrary />
+                <span>Your Library</span>
+            </li>
+        </ul>
+    </div>
+    
+    
+    <Playlist></Playlist>
+    Sidebar</Container>;
+  
 }
 
 const Container = styled.div`
   background-color: black;
+  color: #b3b3b3;
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  width: 100vw;
+  .top_links {
+    display: flex;
+    flex-direction: column;
+    .logo {
+      text-align: center;
+      margin: 1rem 0;
+      img {
+              max-inline-size: 80%; 
+              block-size: auto; 
+           }
+    }
+    ul {
+      list-style-type: none;
+      display: flex;
+      flex-direction: column;
+      gap: 1.5rem;
+      padding: 1rem;
+      li {
+        display: flex;
+        gap: 1rem;
+        cursor: pointer;
+        transition: 0.3s ease-in-out;
+        &:hover {
+          color: white;
+        }
+
+        span {
+          font-size: 1.5rem;
+        }
+      }
+    }
+  }
 `;
