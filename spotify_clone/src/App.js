831f1b68e75ce9d4c1208d295319f66a22706381
arun909 <arunbabu7591@gmail.com>
2024-10-06 17:49:12 +0530

    second phase [login] compleated

--- spotify_clone/src/App.js
+++ spotify_clone/src/App.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import Login from './components/Login';  
+import { useStateProvider } from './utils/StateProvider';
+import { reducerCases } from './utils/Constants';
+import Spotify from './components/Spotify';
 
 export default function App() {
+  // Call the useStateProvider hook correctly by adding parentheses
+  const [{ token }, dispatch] = useStateProvider();
+
+  useEffect(() => {
+    const hash = window.location.hash;
+    if (hash) {
+      const token = hash.substring(1).split('&')[0].split('=')[1];
+      console.log(token);
+      // Dispatch the token using the reducer
+      dispatch({ type: reducerCases.SET_TOKEN, token });
+    }
+  }, [token, dispatch]);
+
   return (
-    <div>App</div>
-  )
+    <div>{ token ? <Spotify /> : <Login /> }</div>
+  );
 }
--- spotify_clone/src/index.js
+++ spotify_clone/src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
 import { StateProvider } from './utils/StateProvider';
 import reducer, { initialState } from './utils/reducer';
 
 
-ReactDOM.render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
    <StateProvider initialState={initialState} reducer={reducer}>
 
