18a099d7553e1ec2ec176126e50ed0aef9dc10e6
arun909 <arunbabu7591@gmail.com>
2024-10-06 21:17:01 +0530

    layout complate(section3)

---
+++ spotify_clone/src/components/Sidebar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'  
+export default function m() {
+  return <Container>Sidebar</Container>;
+    <div>Sidebar</div>
+
+}
+
+const Container = styled.div`
+  background-color: black;
+`;
--- spotify_clone/src/components/Spotify.jsx
+++ spotify_clone/src/components/Spotify.jsx
@@ -1,7 +1,48 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
+import Navbar from './Navbar';
+import Footer from './Footer';
+import Sidebar from './Sidebar';
+import Body from './Body';
 
-export default function  Spotify() {
+export default function Spotify() {
   return (
-    <div> Spotify</div>
-  )
+    <Container>
+      <div className='spotify_body'>
+        <Sidebar />
+        <div className='body'>
+          <Navbar />
+          <div className='body_contents'>
+            <Body />
+          </div>
+        </div>
+      </div>
+      <div className='spotify_footer'>
+        <Footer />
+      </div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  max-width: 100vw;
+  max-height: 100vh;
+  overflow: hidden;
+  display: grid;
+  grid-template-rows: 85vh 15vh;
+
+  .spotify_body {
+    display: grid;
+    grid-template-columns: 15vw 85vw;
+    height: 100%;
+    width: 100%;
+    background: linear-gradient(transparent, rgba(0, 0, 0, 1));
+    background-color: rgb(32, 87, 100);
+  }
+  .bofy {
+    height: 100%;
+    width: 100%;
+    overflow: auto;
+  }
+  
+`;
